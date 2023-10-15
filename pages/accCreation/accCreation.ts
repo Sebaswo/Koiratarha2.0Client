f37352ff995f11ab7134378a686d60d2c3bce5be
Sebaswo <sebastwo@metropolia.fi>
2023-10-15 22:45:53 +0300

    karttaa ja patheja

---
+++ pages/accCreation/accCreation.ts
@@ -0,0 +1,7 @@
+const registerButton = document.querySelector(
+  "#registerButton"
+) as HTMLElement;
+
+registerButton.addEventListener("click", async () => {
+    console.log("TESTII")
+})
--- src/mapFunctions/map.ts
+++ src/mapFunctions/map.ts
@@ -11,7 +11,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 let layerGroup = L.layerGroup();
-const markerGroup = new L.LayerGroup();
+const markerGroup = L.layerGroup();
 
 document.addEventListener('DOMContentLoaded', () => {
   function success(pos: GeolocationPosition) {
@@ -39,17 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
               '#address')!.innerHTML = pointsOfInterest[i].street_address_fi;
           document.querySelector(
               '#city')!.innerHTML = pointsOfInterest[i].address_city_fi;
-          // const targetCrd = {
-          //   latitude: pointsOfInterest[i].latitude,
-          //   longitude: pointsOfInterest[i].longitude,
-          // };
-          // const button = document.querySelector('#navigationBtn');
-          // button!.replaceWith(button!.cloneNode(true));
-          // document.querySelector('#navigationBtn')!.
-          //     addEventListener('click', function() {
-          //       console.log('Klikattu!');
-          //       getRoute(crd, targetCrd);
-          //     });
         });
       }
       map.addLayer(markerGroup)
@@ -124,81 +113,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function addMarker(crd: Coordinates, text: string): L.Marker {
-  return L.marker([crd.latitude, crd.longitude]).addTo(markerGroup).bindPopup(text);
+  const m =  L.marker([crd.latitude, crd.longitude]).addTo(markerGroup).bindPopup(text);
+  markerGroup.addLayer(m);
+  return m;
 }
-
-// function getRoute(lahto: Coordinates, kohde: Coordinates) {
-//   layerGroup.clearLayers();
-//   const routingAPI = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
-//   // GraphQL haku
-//   const haku = `{
-//   plan(
-//     from: {lat: ${lahto.latitude}, lon: ${lahto.longitude}}
-//     to: {lat: ${kohde.latitude}, lon: ${kohde.longitude}}
-//     numItineraries: 1
-//   ) {
-//     itineraries {
-//       legs {
-//         startTime
-//         endTime
-//         mode
-//         duration
-//         distance
-//         legGeometry {
-//           points
-//         }
-//       }
-//     }
-//   }
-// }`;
-
-//   const fetchOptions = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({query: haku}), // GraphQL haku lisätään queryyn
-//   };
-
-  // lähetetään haku
-  // fetch(routingAPI, fetchOptions).then(function(vastaus) {
-  //   return vastaus.json();
-  // }).then(function(tulos) {
-  //   console.log(tulos.data.plan.itineraries[0].legs);
-  //   const googleKoodattuReitti = tulos.data.plan.itineraries[0].legs;
-  //   for (let i = 0; i < googleKoodattuReitti.length; i++) {
-  //     let color = '';
-  //     switch (googleKoodattuReitti[i].mode) {
-  //       case 'WALK':
-  //         color = 'seagreen';
-  //         break;
-  //       case 'BUS':
-  //         color = 'mediumpurple';
-  //         break;
-  //       case 'RAIL':
-  //         color = 'deepskyblue';
-  //         break;
-  //       case 'TRAM':
-  //         color = 'hotpink';
-  //         break;
-  //       default:
-  //         color = 'coral';
-  //         break;
-  //     }
-  //     const reitti = (googleKoodattuReitti[i].legGeometry.points);
-  //     const pisteObjektit = L.Polyline.fromEncoded(reitti).getLatLngs(); // fromEncoded: muutetaan Googlekoodaus Leafletin Polylineksi
-  //     console.log(layerGroup.getLayers());
-  //     layerGroup.addLayer(L.polyline(pisteObjektit).setStyle({
-  //       color,
-  //     }));
-  //     layerGroup.addTo(map);
-  //   }
-  //   map.fitBounds(
-  //       [[lahto.latitude, lahto.longitude], [kohde.latitude, kohde.longitude]]);
-  // }).catch(function(e) {
-  //   console.error(e.message);
-  // });
-// }
 
 function updateLocations(btnChoice: String) {
   const url = `https://www.hel.fi/palvelukarttaws/rest/v4/unit/?ontologyword=${btnChoice}`;
@@ -211,11 +129,7 @@ function updateLocations(btnChoice: String) {
 }
 
 function success2(pos: GeolocationPosition) {
-  map.eachLayer((layer) => {
-    console.log("XXTEST", layer)
-    if (layer['_latlng'] != undefined)
-      layer.remove();
-  });
+  markerGroup.clearLayers();
   layerGroup.clearLayers();
   const crd = pos.coords;
   console.log(crd);
@@ -242,17 +156,6 @@ function success2(pos: GeolocationPosition) {
             '#address')!.innerHTML = pointsOfInterest[i].street_address_fi;
         document.querySelector(
             '#city')!.innerHTML = pointsOfInterest[i].address_city_fi;
-        // const targetCrd = {
-        //   latitude: pointsOfInterest[i].latitude,
-        //   longitude: pointsOfInterest[i].longitude,
-        // };
-        // const button = document.querySelector('#navigationBtn');
-        // button!.replaceWith(button!.cloneNode(true));
-        // document.querySelector('#navigationBtn')!.
-        //     addEventListener('click', function() {
-        //       console.log('Klikattu!');
-        //       getRoute(crd, targetCrd);
-        //     });
       });
     }
   });
